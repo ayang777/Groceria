ee1a0ef7ce51397255929dbd8f5e9fd470a9390f
theangelaluo <theangelaluo@gmail.com>
2020-06-01 21:11:40 -0700

    started on create request

---
+++ Groceria/CreateRequestScreen.swift
@@ -0,0 +1,30 @@
+//
+//  CreateRequestScreen.swift
+//  Groceria
+//
+//  Created by Angela Luo on 6/1/20.
+//  Copyright © 2020 Angela Luo. All rights reserved.
+//
+
+import UIKit
+
+class CreateRequestScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
