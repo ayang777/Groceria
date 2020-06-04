4cd9b2d023e226a6dae7742866759ac5ad117dd2
ayang777 <anna.yang077@gmail.com>
2020-06-04 00:40:33 -0700

    friends/notifs change button and click on friend

---
+++ Groceria/Profile/SingleFriendViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SingleFriendViewController.swift
+//  Groceria
+//
+//  Created by Anna Yang on 6/4/20.
+//  Copyright © 2020 Angela Luo. All rights reserved.
+//
+
+import UIKit
+
+class SingleFriendViewController: UIViewController {
+
+    @IBAction func deleteFriend(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
